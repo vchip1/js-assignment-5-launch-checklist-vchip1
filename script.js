feb92204199662d79a6b0370b51226843174d657
vchip1 <andy.volding@gmail.com>
2022-12-06 20:59:48 -0600

    second push

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   
+   let listedPlanetsResponse=myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let newPlanet=pickPlanet(listedPlanets);
+addDestinationInfo(document, newPlanet.name, newPlanet.dismeter, newPlanet.star, newPlanet.distance, newPlanet.moons, newPlanet.image);
    })
-   
+   document.getElementNyId("faultyItems").style.visibility="hidden";
+   let form=document.querySelector("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let pilot=document.querySelector("input[name=pilotName]").value
+    let copilot=document.querySelector("input[name=copilotName]").value
+    let fuelLevel=document.querySelector("input[name=fuelLevel]").value
+    let cargoMass=document.querySelector("input[name=cargoMass]").value
+
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+})
+
+
+
 });
