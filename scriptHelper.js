93f325193573bec01bb9eeda05e1c1b142d2179b
vchip1 <andy.volding@gmail.com>
2022-12-06 20:07:11 -0600

    fist push

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,7 +2,7 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
+   //Here is the HTML formatting for our mission target div.
    /*
                 <h2>Mission Destination</h2>
                 <ol>
@@ -17,11 +17,52 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (testInput = "") {
+    return "Empty";
+   } else if (isNaN(testInput)){
+    return "Not a Number";
+   } else if (isNaN(testInput)===false) {
+    return "Is a Number";
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   let pilotStatus=document.getElementById("pilotStatus");
+   let copilotStatus=document.getElementById("copilotStatus");
+   let fuel=document.getElementById("fuelStatus");
+   let cargo=document.getElementById("cargoStatus");
+
+   if (validateInput(pilot)==="Empty" || validateInput(copilot)==="Empty" || validateInput(fuelLevel)==="Empty" || validateInput(cargoLevel)==="Empty"){
+    alert("Error: One or more fields are blank. Please complete all fields.")
+   } else if (validateInput(pilot)==="Is a Number" || validateInput(copilot)==="Is a Number" || validateInput(fuelLevel)==="Not a Number" || validateInput(cargoLevel)==="Not a Number"){
+    alert("Error: One or more fields contains an invalid entry.")
+   } else {
+    faultyItems.style.visibility="visible";
+    pilotStatus.innerHTML=`${pilot} is prepared for launch!`
+    copilotStatus.innerHTML=`${copilot} is also prepared for launch!`
+    let launchStatus=document.getElementById("launchStatus");
+    if ((fuelLevel<10000) && (cargoLevel<= 10000)){
+launchStatus.innerHTML="Shuttle not ready for launch"
+launchStatus.style.color="Red"
+        fuelStatus.innerHTML="Not enough fuel for launch"
+cargoStatus.innerHTML="Mass low enough for launch"
+    } else if ((fuelLevel>=10000) && (cargoLevel> 10000)){
+        launchStatus.innerHTML="Shuttle not ready for launch"
+        launchStatus.style.color="Red"
+        fuelStatus.innerHTML="Fuel level sufficient for launch"
+        cargoStatus.innerHTML="Mass too high for launch"
+    } else if ((fuelLevel<10000) && (cargoLevel>10000)){
+        launchStatus.innerHTML="Shuttle not ready for launch"
+        launchStatus.style.color="Red"
+        fuelStatus.innerHTML="Not enough fuel for launch"
+        cargoStatus.innerHTML="Mass too high for launch"
+    } else {
+        launchStatus.innerHTML="Shuttle not ready for launch"
+        launchStatus.style.color="Green"
+        fuelStatus.innerHTML="Fuel level sufficient for launch"
+        cargoStatus.innerHTML="Mass low enough for launch"
+    }
+   }
 }
 
 async function myFetch() {
